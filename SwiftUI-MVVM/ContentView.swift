c9d4f93c83299ba980d947ec99aa02a0074dacb0
cesatia <carloseduardosantiago@correio.itau.com.br>
2019-08-20 20:04:37 -0300

    Initial Commit

---
+++ SwiftUI-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM
+//
+//  Created by Carlos Eduardo Santiago on 20/08/19.
+//  Copyright © 2019 Carlos Eduardo Santiago. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
